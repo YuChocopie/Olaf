7cb6c80c13d860bf3633d2d6b7b96ca99fc87c3a
pengin7384 <pengin7384@gmail.com>
2018-12-03 22:50:35 +0900

    Change remark

--- app/src/main/java/com/mashupgroup/weatherbear/models/weather/Weather.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/models/weather/Weather.kt
@@ -1,50 +1,31 @@
 package com.mashupgroup.weatherbear.models.weather
 
-/*
-# Weather Data Class
-
-coord.lon               : 경도
-coord.lat               : 위도
-weather[0].id           : 날씨 아이디
-weather[0].main         : 날씨 String(Ex. Clear)
-weather[0].description  : 날씨 설명 String (Ex. clear sky)
-weather[0].icon         : 날씨 아이콘 번호
-base                    : Internal parameter
-main.temp               : Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
-main.humidity           : 습도 %
-main.pressure           : 대기압
-main.temp_min           : 순간 최저 온도
-main.temp_max           : 순간 최대 온도
-main.seaLevel           : 해수면 대기압, hPa
-main.grndLevel          : 지면위의 대기압, hPa
-wind.speed              : 풍속. 단위 met/sec
-wind.deg                : 풍향
-clouds.all              : 흐림 %
-rain.1h                 : 지난 1시간 동안의 강수량 (문제 있음)
-rain.3h                 : 지난 3시간 동안의 강수량 (문제 있음)
-dt                      : 시간, unix, UTC
-sys.type                : Internal parameter
-sys.id                  : Internal parameter
-sys.message             : Internal parameter
-sys.country             : 나라 코드(Ex. GB, JP)
-sys.sunrise             : 일출 시간, unix, UTC
-sys.sunset              : 일몰 시간, unix, UTC
-id                      : 도시 아이디
-name                    : 도시 이름
-cod                     : Internal parameter
- */
-
 data class Weather(
+    /** lon:경도, lat:위도 */
     var coord : Coord,
+    /** id:아이디, main:날씨 String, description:설명 String */
     var weather: List<WeatherData>,
+    /** Internal parameter */
     var base: String,
+    /** temp:온도, humidity:습도%, pressure:대기압,
+     *  temp_min:최저 온도, temp_max:최대 온도,
+     *  seaLevel:해수면 대기압 hPa, grndLevel:지면위의 대기압 hPa */
     var main: Main,
+    /** speed:풍속, deg:풍향 */
     var wind: Wind,
+    /** all:흐림% */
     var clouds: Clouds,
+    /** 1h:지난 1시간 동안의 강수량, 3h:지난 3시간 동안의 강수량(문제 있음) */
     var rain: Rain,
+    /** UTC unix 시간 */
     var dt: Int,
+    /** Internal parameter */
     var sys: Sys,
+    /** 도시 아이디 */
     var id: Int,
+    /** 도시 이름 */
     var name: String,
+    /** Internal parameter */
     var cod: Int
 )
+
