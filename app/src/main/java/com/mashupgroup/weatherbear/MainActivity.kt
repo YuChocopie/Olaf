452a8c1e5d40889a048e48978d180d69dc74b72e
Kim Yujeong <dbwjd@users.noreply.github.com>
2018-12-10 03:37:26 +0900

    Bear controlling class (#42)
    * Bear controlling class
    
    - BearModel생성해 곰의 모습 컨트롤
    - DataBinding 적용
    - 파일및 string name 변경
    
    Fisex https://github.com/dbwjd/Olaf/issues/38
    
    * - renaming Ane Use enum

--- app/src/main/java/com/mashupgroup/weatherbear/MainActivity.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/MainActivity.kt
@@ -10,6 +10,10 @@ import retrofit2.Retrofit
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.bear.*
+import android.databinding.DataBindingUtil.setContentView
+import android.databinding.ViewDataBinding
+import kotlinx.android.synthetic.main.item_oneday_weather.*
+
 
 class MainActivity : AppCompatActivity() {
     val OpenWeatherMapKey = "926ab2fea6549951c324d1dc64014bdb"
@@ -17,6 +21,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding: ViewDataBinding = setContentView(this, R.layout.activity_main)
+        binding.setVariable(BR.bear,BearViewModel())
+
 
         requestTodayWeather()
 
@@ -24,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         val bear_ear_l: Animation = AnimationUtils.loadAnimation(this, R.anim.bear_ear_left)
         val leg_frong: Animation = AnimationUtils.loadAnimation(this, R.anim.leg_frong)
         val leg_frong_good: Animation = AnimationUtils.loadAnimation(this, R.anim.leg_frong_good)
-        val leg_frong_bed: Animation = AnimationUtils.loadAnimation(this, R.anim.leg_frong_bed)
+        val leg_frong_bad: Animation = AnimationUtils.loadAnimation(this, R.anim.leg_frong_bad)
         val umbrella: Animation = AnimationUtils.loadAnimation(this, R.anim.umbrella)
         val bear_mask: Animation = AnimationUtils.loadAnimation(this, R.anim.bear_mask)
         val scarh_high: Animation = AnimationUtils.loadAnimation(this, R.anim.scarf_high)
@@ -42,31 +49,14 @@ class MainActivity : AppCompatActivity() {
         // 비올 때 우산과 팔 동시움직임
         bear_umbrella.startAnimation(umbrella)
         //미세먼지 농도 높을 때 마스크와 마스크 발 동시.
-        bear_leg_front_bed.startAnimation(leg_frong_bed)
+        bear_leg_front_bad.startAnimation(leg_frong_bad)
         handkerchief.startAnimation(bear_mask)
         //먼지 펫 움직이기
         bear_pet.startAnimation(pet)
         bear_pet_small.startAnimation(pet)
         bear_pet_w.startAnimation(pet)
 
-        /*
-        미세먼지 단계 4단계
-            - 곰의 색
-            - 눈의 색
-            - 앞발의 변경(1,2단계:leg_frong_good 3,4단계: leg_frong_bed + 마스크)
-            - 표정의 변경(1,2   : good            3,4 : bad)
-            - 옆의 펫의 여부(4단계에만 나타남)
 
-        날씨에 따른 변화
-            - 비 : 곰 손에 우산 추가 ++ 미세먼지 앞발 변하지 않음. leg_frong_good 로 유지
-                   배경에 비 내림
-            - 눈 : 배경에 눈 내림
-            - 폭설 : 배경에 눈 내림 + 눈사람으로 변경
-            - 구름, 바람 : 배경에 구름 지나감
-            - 맑음 : 그냥 맑음
-
-        시간
-            - 밤/낮 : 곰의 얼굴 변경 //밤에 무조건 잠. */
     }
 
     fun requestTodayWeather() {
@@ -82,7 +72,7 @@ class MainActivity : AppCompatActivity() {
                 .subscribe({ weather : Weather? ->
                     if(weather != null) {
                         // 현재 기온
-                        tvTodayWeatherTemperature.text = String.format("%.1f",(weather.main.temp-273))
+                        tvDayWeatherTemperature.text = String.format("%.1f",(weather.main.temp-273))
                     }
                 }, {
                     error -> error.printStackTrace()
