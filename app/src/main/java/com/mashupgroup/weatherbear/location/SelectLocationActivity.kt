80ded32edebdcacf4e2bb96e61d579cbfe783380
Jujin <jujin@jujinkim.com>
2018-12-15 01:02:23 +0900

    Added 'select current location' activity

---
+++ app/src/main/java/com/mashupgroup/weatherbear/location/SelectLocationActivity.kt
@@ -0,0 +1,13 @@
+package com.mashupgroup.weatherbear.location
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.mashupgroup.weatherbear.R
+
+class SelectLocationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_location)
+    }
+}
