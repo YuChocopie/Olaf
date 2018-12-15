e4272b54465289fb56d1d83a63518cc30ecb44c8
Jujin Kim <jk012345@gmail.com>
2018-12-15 20:46:50 +0900

    Added search location activity, and connected it to select location activity. Not implemented yet.

--- app/src/main/java/com/mashupgroup/weatherbear/location/SelectLocationActivity.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/location/SelectLocationActivity.kt
@@ -1,13 +1,34 @@
 package com.mashupgroup.weatherbear.location
 
+import android.app.Activity
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.mashupgroup.weatherbear.R
+import kotlinx.android.synthetic.main.activity_select_location.*
 
 class SelectLocationActivity : AppCompatActivity() {
+    val RESULT_CODE_SEARCH_LOCATION_ACTIVITY = 111
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_location)
+
+        fabtnAddLocation.setOnClickListener {onSearchLocationFABtnClicked() }
+    }
+
+    private fun onSearchLocationFABtnClicked() {
+        val intent = Intent(this, SearchLocationActivity::class.java)
+        startActivityForResult(intent, RESULT_CODE_SEARCH_LOCATION_ACTIVITY)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == RESULT_CODE_SEARCH_LOCATION_ACTIVITY) {
+            if(resultCode == Activity.RESULT_OK) {
+                // 검색 성공 및 결과 받아옴 (data intent안에 결과 있어야함)
+            } else {
+                // 검색 실패. 실패 메시지 띄워야함
+            }
+        }
     }
 }
