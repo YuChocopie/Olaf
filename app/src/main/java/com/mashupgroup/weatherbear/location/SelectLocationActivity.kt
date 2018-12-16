1ad07dd2be55d726f665172af062a8cdcdfcb560
Jujin <jujin@jujinkim.com>
2018-12-17 01:11:28 +0900

    Added 'search location and add' feature completely (except GUI)

--- app/src/main/java/com/mashupgroup/weatherbear/location/SelectLocationActivity.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/location/SelectLocationActivity.kt
@@ -2,8 +2,10 @@ package com.mashupgroup.weatherbear.location
 
 import android.app.Activity
 import android.content.Intent
+import android.location.Address
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.mashupgroup.weatherbear.R
 import kotlinx.android.synthetic.main.activity_select_location.*
 
@@ -26,8 +28,14 @@ class SelectLocationActivity : AppCompatActivity() {
         if(requestCode == RESULT_CODE_SEARCH_LOCATION_ACTIVITY) {
             if(resultCode == Activity.RESULT_OK) {
                 // 검색 성공 및 결과 받아옴 (data intent안에 결과 있어야함)
+                data?.let { intent ->
+                    val lat = intent.getDoubleExtra("lat", .0)
+                    val long = intent.getDoubleExtra("long", .0)
+                    val name = intent.getStringExtra("name")
+                    Log.v("JUJINTEST", "$name, $lat, $long")
+                }
             } else {
-                // 검색 실패. 실패 메시지 띄워야함
+                // 검색 실패 또는 취소.
             }
         }
     }
