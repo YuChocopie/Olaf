2eba9528a402cb8a04b2337bce7388f805baf7e7
pengin7384 <pengin7384@gmail.com>
2018-12-27 05:23:34 +0900

    Modify AirItem
    Fixes #109

--- app/src/main/java/com/mashupgroup/weatherbear/models/air/AirItem.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/models/air/AirItem.kt
@@ -1,7 +1,13 @@
 package com.mashupgroup.weatherbear.models.air
 
 data class AirItem(
-        var cityName: String,
+        var dataTime: String,
         var pm10Value: String,
-        var pm25Value: String
+        var pm10Value24: String,
+        var pm25Value: String,
+        var pm25Value24: String,
+        var pm10Grade: String,
+        var pm25Grade: String,
+        var pm10Grade1h: String,
+        var pm25Grade1h: String
 )
