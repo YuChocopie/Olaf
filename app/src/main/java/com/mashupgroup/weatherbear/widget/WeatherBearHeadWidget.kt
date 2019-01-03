1810fab00e8d617983ae580a67890653c4d4731c
Kim Yujeong <dbwjd@users.noreply.github.com>
2019-01-03 21:08:25 +0900

    Add a widget, changing the widget renewal interval to 30 minutes (#114)
    - 위젯 갱신주기 변경
    - 위젯 클릭할 경우 메인액티비티로 전환
    - 위젯대표이미지 변경
    - arrow 아이콘 생성
    - 배경유무에 따른 위젯 추가생성
    
    Fixes https://github.com/dbwjd/Olaf/issues/113

--- app/src/main/java/com/mashupgroup/weatherbear/widget/WeatherBearHeadWidget.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/widget/WeatherBearHeadWidget.kt
@@ -1,11 +1,15 @@
 package com.mashupgroup.weatherbear.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+import com.mashupgroup.weatherbear.MainActivity
 
 import com.mashupgroup.weatherbear.R
+import java.util.*
 
 /**
  * Implementation of App Widget functionality.
@@ -13,10 +17,11 @@ import com.mashupgroup.weatherbear.R
 class WeatherBearHeadWidget : AppWidgetProvider() {
 
     override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        var weather = "HEAVY_SNOW"
+        var fineDustLevel = "좋음"
         // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
-        }
+        for (appWidgetId in appWidgetIds) updateAppWidget(context, appWidgetManager, appWidgetId,
+                weather, fineDustLevel)
     }
 
     override fun onEnabled(context: Context) {
@@ -30,40 +35,51 @@ class WeatherBearHeadWidget : AppWidgetProvider() {
     companion object {
 
         internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
-                                     appWidgetId: Int) {
+                                     appWidgetId: Int, nWeather: String, nFineDustLevel: String) {
 
 
             //SNOW, RAINY, THUNDER_RAINY, WIND, CLOUD, SUNNY, HEAVY_SNOW
-            //낮:true 밤:false
             //미세먼지 : 좋음,보통,나쁨.매우나쁨
-            var weather = "HEAVY_SNOW"
+            //낮:true 밤:false
+            val weather = nWeather
+            val fineDustLevel = nFineDustLevel
             var time = true
-            var fineDustLevel = "좋음"
+            val date = Date().hours
+            time = date in 6..17
 
             var bearSkin = R.drawable.ic_bear_head_01
             var bearSnow: Int
             var bearFace: Int
 
-            when(fineDustLevel){
-                "좋음" -> bearSkin=(R.drawable.ic_bear_head_01)
-                "보통" -> bearSkin=(R.drawable.ic_bear_head_02)
-                "나쁨" -> bearSkin=(R.drawable.ic_bear_head_03)
-                "매우나쁨" -> bearSkin=(R.drawable.ic_bear_head_04)
+            //홈스크린의 위젯 xml에 대한 인스턴스를 받아온다,
+            val remoteViews = RemoteViews(context.packageName, R.layout.weather_bear_head_widget)
+            //main activity와 위젯간 데이터 교환의 매개채
+            val intent = Intent(context, MainActivity::class.java)
+            val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+            remoteViews.setOnClickPendingIntent(R.id.widgetBearHead, pendingIntent)
+            appWidgetManager.updateAppWidget(appWidgetId, remoteViews)
+
+            when (fineDustLevel) {
+                "좋음" -> bearSkin = (R.drawable.ic_bear_head_01)
+                "보통" -> bearSkin = (R.drawable.ic_bear_head_02)
+                "나쁨" -> bearSkin = (R.drawable.ic_bear_head_03)
+                "매우나쁨" -> bearSkin = (R.drawable.ic_bear_head_04)
             }
 
             if (time) {
-                bearFace=R.drawable.ic_msg_bear_face_good
-                if(fineDustLevel=="나쁨" || fineDustLevel == "매우나쁨") {
-                    bearFace=R.drawable.ic_msg_bear_face_bad
+                bearFace = R.drawable.ic_msg_bear_face_good
+                if (fineDustLevel == "나쁨" || fineDustLevel == "매우나쁨") {
+                    bearFace = R.drawable.ic_msg_bear_face_bad
                 }
                 if (weather == "SNOW" || weather == "HEAVY_SNOW") {
-                    bearSnow=(R.drawable.ic_msg_bear_head_snow)
+                    bearSnow = (R.drawable.ic_msg_bear_head_snow)
                     if (fineDustLevel == "좋음") {
-                        bearSnow=(R.drawable.ic_msg_bear_head_snow_good)
-                } }else
-                    bearSnow=(R.drawable.ic_msg_bear_head_snow_none)
+                        bearSnow = (R.drawable.ic_msg_bear_head_snow_good)
+                    }
+                } else
+                    bearSnow = (R.drawable.ic_msg_bear_head_snow_none)
             } else {
-                bearFace=R.drawable.ic_msg_bear_face_sleep
+                bearFace = R.drawable.ic_msg_bear_face_sleep
                 bearSnow = (R.drawable.ic_msg_bear_head_snow_none)
             }
 
