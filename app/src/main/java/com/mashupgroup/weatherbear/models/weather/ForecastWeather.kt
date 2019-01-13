a83e50f20891da3d33909eaee0281d3253fbbc1e
dbwjd <rladbwjd341>
2019-01-14 04:59:49 +0900

    Get the temperature of tomorrow and body temperature calculation
    - 내일 정오시간의 날씨와 온도 받아오기
    - 체감온도 계산 적용

--- app/src/main/java/com/mashupgroup/weatherbear/models/weather/ForecastWeather.kt
+++ app/src/main/java/com/mashupgroup/weatherbear/models/weather/ForecastWeather.kt
@@ -2,6 +2,7 @@ package com.mashupgroup.weatherbear.models.weather
 
 data class ForecastWeather(
         var dt: String,
-        var main : ForecastWeatherMain,
-        var weather: List<WeatherData>
+        var main: ForecastWeatherMain,
+        var weather: List<WeatherData>,
+        var wind: WindData
 )
